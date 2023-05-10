3065dad62294789dd01f6d85e1c9c337552bfc5e
Ankit Singh Chouhan <ankitt335407@gmail.com>
2023-05-10 19:05:22 +0530

    Marker

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import L from "leaflet";
+import "leaflet/dist/leaflet.css";
+import "./App.css";
 function App() {
+  const mapURL = "./map.png";
+  const customIcon = L.icon({
+    iconUrl: "./icon.png",
+    iconSize: [50, 55], // size of the icon
+    iconAnchor: [25, 25], // point of the icon which will correspond to marker's location
+  });
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <MapContainer
+      center={[51.505, -0.09]}
+      zoom={13}
+      scrollWheelZoom={true}
+      style={{ height: "100vh", width: "100vw" }}
+    >
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        // url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        url={mapURL}
+        className="tile-layer"
+      />
+      <Marker icon={customIcon} position={[51.505, -0.09]}>
+        <Popup>
+          A pretty CSS3 popup. <br /> Easily customizable.
+        </Popup>
+      </Marker>
+    </MapContainer>
   );
 }
 
